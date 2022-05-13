2e60ae0b7f6065c4ce0a371421b920c21e842388
Ayush <ayushupadhyay456@gmail.com>
2022-05-13 22:36:25 +0530

    third commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import Header from './Components/Header';
-import Api from './Components/Api';
+
 
 
 function App() {
   return (
     <>
     <Header/>
-    <Api/>
+    
     </>
   );
 }
--- src/Components/Api.js
+++ src/Components/Api.js
@@ -1,66 +1,60 @@
 import React from 'react'
-import {useState, useEffect} from 'react'
-import { Row } from 'react-bootstrap'
+import { useState, useEffect } from 'react'
+import { Row, Col } from 'react-bootstrap'
 import Card from 'react-bootstrap/Card'
 import axios from 'axios'
+import Cards from './Cards'
 import './style.css'
 
 
-function Api(){
-    
-    const [dt,setData]=useState([])
+function Api() {
 
-    useEffect(()=>{
+    const [dt, setData] = useState([])
+
+    useEffect(() => {
         async function fetchData() {
-            const req = await axios.get("https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=a&numResults=10");
+            const req = await axios.get("https://asia-south1-socialboat-dev.cloudfunctions.net/assignmentVideos?q=a&numResults=50");
             setData(req.data.results);
-        
+
         }
         fetchData();
-        console.log(dt);
-    }, []);
-    
 
-    
+    }, []);
+
+
+
     return (
-        
-       <div>
-           <h1>Get api call</h1>
+
+
+        <div className = "main">
+            
             {
-             dt.map(element=>
+
+                dt.map(element =>
                 (
-             <div className='cd'>
-             
-             
-            
-             
-            
-             <Card className='Card-grp mb-4' style={{ width: '18rem',border:"none",}}>
-             <div className='upper-video'>
-             <h2 align="center">{element.heading}</h2>
-             <video  width="287px" height="200px" controls autoplay>
-                 <source src={element.video} type="video/mp4"/>
-                 Your browser does not support the video tag
-             </video>
-             
-             </div>
-             <ul>
-             <li>{element.tags[0]}</li>
-             <li>{element.tags[1]}</li>
-             <li>{element.tags[2]}</li>
-             </ul>
-            
-  </Card>
- 
-            
-             
-             
-            </div>
-             ))} 
-       </div>
-       
-        
-)
+                    <div className='cd'>
+                        <Card className='Card-grp mb-4' style={{ width: '18rem', border: "none", }}>
+                            <div className='upper-video'>
+                                <h2 align="center">{element.heading}</h2>
+                                <video width="287px" height="200px" controls>
+                                    <source src={element.video} type="video/mp4" />
+                                    Your browser does not support the video tag
+                                </video>
+
+                            </div>
+                            <ul>
+                                <li>{element.tags[0]}</li>
+                                <li>{element.tags[1]}</li>
+                                <li>{element.tags[2]}</li>
+                            </ul>
+
+                        </Card>
+                    </div>
+                ))}
+        </div>
+
+
+    )
 }
 export default Api;
 
